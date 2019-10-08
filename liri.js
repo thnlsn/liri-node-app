dd742a8b25b49062a86eea01758d942c736f38eb
thnlsn <52093425+thnlsn@users.noreply.github.com>
2019-10-07 19:41:53 -0700

    added links to spotify API

--- liri.js
+++ liri.js
@@ -6,6 +6,30 @@ var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
 
+var search = process.argv[2];
+console.log()
+var term = process.argv.slice(3).join(" ");
+
+//////////////////////////////////////////////////////////////////////
+// 
+//////////////////////////////////////////////////////////////////////
+
+if (!search) {
+  search = "spotify-this-song";
+}
+if (!term) {
+  term = "The Sign";
+}
+
+if (search === "spotify-this-song") {
+  console.log("Searching for the artist, song name, a preview link, and the album");
+  tv.findShow(term);
+} else {
+  tv.findActor(term);
+  console.log("Searching for TV Actor...");
+}
+
+
 
 
 
