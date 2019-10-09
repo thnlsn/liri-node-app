4b9a4819a09de9e540b3ce34c9bf0c33ef65667d
thnlsn <52093425+thnlsn@users.noreply.github.com>
2019-10-08 19:44:11 -0700

    finished movie-this section

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # liri-node-app
+
+The concert-this and movie-this sections work perfectly. The spotify-this-song section is having issues with the Spotify constructor.
--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+var fs = require("fs");
+
 var keys = require("./keys.js");
 
 var axios = require("axios");
@@ -24,10 +26,12 @@ if (search === "concert-this") {
 } else if (search === "spotify-this-song") {
 /*   tv.findActor(term); // CHANGE */
   console.log("Searching for the artist, song name, a preview link, and the album...");
+  spotify(term);
 
 } else if (search === "movie-this") {
 /*   tv.findActor(term); // CHANGE */
   console.log("Searching for movie title, release year, IMDB rating, Rotten Tomatoes rating, producing country, language, plot, and actors...");
+  movie(term);
 
 } else if (search === "do-what-it-says") {
 /*   tv.findActor(term); // CHANGE */
@@ -45,45 +49,38 @@ function concert(artistName) {
   axios.get("http://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp").then(
   function(response) {
 
-    console.log("Venue Name: " + response.data[0].venue.name);
-    console.log("Location: " + response.data[0].venue.city);
-    console.log("Time: " + response.data[0].datetime);
+    console.log("Venue Name: " + response.data[0].venue.name + "\nLocation: " + response.data[0].venue.city + "\nTime: " + response.data[0].datetime);
 /*     console.log(moment(response.data[0].datetime).format("MM/DD/YYYY")); */
   }
-)}
-
-
-
-
-
+)};
 
 //////////////////////////////////////////////////////////////////////
 // SPOTIFY-THIS-SONG
 //////////////////////////////////////////////////////////////////////
 
-if (!search) {
-  search = "spotify-this-song";
-}
-if (!term) {
-  term = "The Sign";
-}
+function spotify(songName) {
+  if (!search) {
+    search = "spotify-this-song";
+  }
+  if (!term) {
+    term = "The Sign";
+  }
 
+  var spotify = new Spotify({
+    id: spotifyKeyInfo["spotify"].id,
+    secret: spotifyKeyInfo["spotify"].secret
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  spotify.request('https://api.spotify.com/v1/search?q=track:' + songName + '&type=track&limit=10', function(error, response) {
+      if (error){
+          return console.log(error);
+      };
+      console.log("Artist: " + response.tracks.items[0].artists[0].name);
+      console.log("Song: " + response.tracks.items[0].name);
+      console.log("URL: " + response.tracks.items[0].preview_url);
+      console.log("Album: " + response.tracks.items[0].album.name);
+  });
+};
 
 
 
@@ -91,6 +88,11 @@ if (!term) {
 // MOVIE-THIS
 //////////////////////////////////////////////////////////////////////
 
+function movie(movieName) {
+axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(
+  function(movieResponse){
+      console.log("Title: " + movieResponse.data.Title + "\nRelease Year: " + movieResponse.data.Year + "\nIMDB Rating: " + movieResponse.data.imdbRating + "\nCountry: " + movieResponse.data.Country + "\nLanguage: " + movieResponse.data.Language + "\nPlot: " + movieResponse.data.Plot + "\nActors: " + movieResponse.data.Actors + "\nRotten Tomatoes Rating: " + movieResponse.data.Ratings[1].Value);
+})};
 
 
 
@@ -109,65 +111,4 @@ if (!term) {
 //////////////////////////////////////////////////////////////////////
 // DO-WHAT-IT-SAYS
 //////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var axios = require("axios");
-var fs = require("fs");
-
-// Create the TV constructor
-var TV = function() {
-  // divider will be used as a spacer between the tv data we print in log.txt
-  var divider = "\n------------------------------------------------------------\n\n";
-
-  // findShow takes in the name of a tv show and searches the tvmaze API
-  this.findShow = function(show) {
-    var URL = "http://api.tvmaze.com/singlesearch/shows?q=" + show;
-
-    axios.get(URL).then(function(response) {
-      // Place the response.data into a variable, jsonData.
-      var jsonData = response.data;
-
-      // showData ends up being the string containing the show data we will print to the console
-      var showData = [
-        "Show: " + jsonData.name,
-        "Genre(s): " + jsonData.genres.join(", "),
-        "Rating: " + jsonData.rating.average,
-        "Network: " + jsonData.network.name,
-        "Summary: " + jsonData.summary
-      ].join("\n\n");
-
-      // Append showData and the divider to log.txt, print showData to the console
-      fs.appendFile("log.txt", showData + divider, function(err) {
-        if (err) throw err;
-        console.log(showData);
-      });
-    });
-  };
-
-  this.findActor = function(actor) {
-    var URL = "http://api.tvmaze.com/search/people?q=" + actor;
-
-    // Add code to search the TVMaze API for the given actor
-    // The API will return an array containing multiple actors, just grab the first result
-    // Append the actor's name, birthday, gender, country, and URL to the `log.txt` file
-    // Print this information to the console
-  };
-};
-
-module.exports = TV;
+console.log("WEWEWEWEWEWEWE#@#@#@#@#@#@");
